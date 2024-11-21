ede63c5fe691160b547ae6ef2e401610774c8872
deepikapatil3112 <deepika.patil@theadroit.in>
2024-11-21 13:28:13 +0530

    Added Clients Section
    Added Clients Section.

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.clients-logo-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 4,
+    });
+})
