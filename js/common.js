81a41278c5642c81048030b29ffe4323eadc8669
deepikapatil3112 <deepika.patil@theadroit.in>
2024-11-22 13:34:57 +0530

    Added the remaining sections and css.
    Added the responsive for the Index Page.

--- js/common.js
+++ js/common.js
@@ -2,9 +2,19 @@ $(document).ready(function(){
     $('.clients-logo-slider').slick({
         dots: true,
         arrows: false,
-        infinite: false,
+        infinite: true,
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
     });
+
+    $(window).scroll(function () {
+        var scroll = $(this).scrollTop();
+        if (scroll > 100) {
+          $(".header").addClass("active");
+        }
+        else {
+          $(".header").removeClass("active");
+        }
+      });
 })
