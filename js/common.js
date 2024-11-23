149665f3e894e12c02acb3e5a15750b90a7a9eba
deepikapatil3112 <deepika.patil@theadroit.in>
2024-11-23 14:47:56 +0530

    Responsive for Index Page
    Responsive for Index Page.

--- js/common.js
+++ js/common.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
-    $('.clients-logo-slider').slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    });
 
-    $(window).scroll(function () {
-        var scroll = $(this).scrollTop();
-        if (scroll > 100) {
-          $(".header").addClass("active");
-        }
-        else {
-          $(".header").removeClass("active");
-        }
-      });
+  // Header Background Color Change on Scroll
+  $(window).scroll(function () {
+    var scroll = $(this).scrollTop();
+    if (scroll > 100) {
+      $(".header").addClass("active");
+    }
+    else {
+      $(".header").removeClass("active");
+    }
+  });
+
+  // Hamburger Menu
+  var headerH = $(".header").outerHeight()
+
+  $(document).on("click", ".menu-btn", function(){
+    $(this).toggleClass("active")
+    $(".header .navbar-links").toggleClass("active")
+    $(".header .navbar-links").css('top' , headerH)
+  })
+
+  // Clients Logo Slider
+  $('.clients-logo-slider').slick({
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+  });
+
 })
